c3c9a4eeb1161c53e953627b56957b60c399fcff
Raphael-Hernandez <raphaelhrndpro@gmail.com>
2023-01-05 10:33:07 +0100

    etape 1.4

--- Humain.php
+++ Humain.php
@@ -6,6 +6,14 @@ class Humain
     public $taille = 175;
     public $nom;
 
+    public function __construct(){
+        echo "Je suis né.e \n";
+    }
+
+    public function __destruct(){
+        echo "Je suis mort.e\n";
+    }
+
     public function marcher()
     {
         echo "Je marche \n";
@@ -15,10 +23,11 @@ class Humain
     {
         echo $this->taille += 1;
     }
-
 }
 $marcelline = new Humain();
 $constance = new Humain();
 
 echo 'La taille de Marcelline et de '.$marcelline->taille." cm \n";
 echo 'La taille de Constance et de '.$constance ->taille." cm \n";
+
+unset($constance);
